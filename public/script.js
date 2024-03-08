62a26c8e96b1e83e2479e69d2a2aa49a43e9ffc5
Biran Capellan-Santos <Brian000315285@RM.lan>
2024-03-08 13:30:30 -0500

    Adding intro animation

---
+++ public/script.js
@@ -0,0 +1,10 @@
+toggler = document.getElementById("toggler");
+toggler.addEventListener("click", expandCircle);
+
+function expandCircle() {
+  const circle = document.getElementById("circle");
+  const rocket = document.getElementById("rocket");
+
+  circle.classList.toggle("expand");
+  rocket.classList.toggle("launch");
+}
